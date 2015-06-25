aef7426f7d905175ed9768bebbe7e2273e6a19ac
Christian Fobel <christian@fobel.net>
2015-06-25 16:32:47 -0400

    Add rpc_module to pavement...
    Add `rpc_module` to pavement, required for `base_node_rpc>=0.5`.

--- pavement.py
+++ pavement.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 import sys
+from importlib import import_module
 
 from paver.easy import task, needs, path, sh, cmdopts, options
 from paver.setuputils import setup, find_package_data, install_distutils_tasks
@@ -10,12 +11,12 @@ except ImportError:
 
 sys.path.insert(0, '.')
 import version
-
 install_distutils_tasks()
 
 DEFAULT_ARDUINO_BOARDS = ['uno', 'mega2560']
 PROJECT_PREFIX = [d for d in path('.').dirs()
                   if d.joinpath('Arduino').isdir()][0].name
+rpc_module = import_module(PROJECT_PREFIX)
 VERSION = version.getVersion()
 URL='http://github.com/wheeler-microfluidics/%s.git' % PROJECT_PREFIX
 PROPERTIES = OrderedDict([('name', PROJECT_PREFIX),
@@ -26,6 +27,7 @@ package_files = find_package_data(package=PROJECT_PREFIX, where=PROJECT_PREFIX,
                                   only_in_packages=False)
 
 options(
+    rpc_module=rpc_module,
     PROPERTIES=PROPERTIES,
     DEFAULT_ARDUINO_BOARDS=DEFAULT_ARDUINO_BOARDS,
     setup=dict(name='wheeler.' + PROJECT_PREFIX,
@@ -35,6 +37,6 @@ options(
                author_email='christian@fobel.net',
                url=URL,
                license='GPLv2',
-               install_requires=['wheeler.base_node_rpc>=0.4'],
+               install_requires=['wheeler.base_node_rpc>=0.5'],
                packages=[PROJECT_PREFIX],
                package_data=package_files))
