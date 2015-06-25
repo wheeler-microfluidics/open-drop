c61d7e162be5a9c3506a9d36eafd997a0c2ee837
Christian Fobel <christian@fobel.net>
2015-06-25 11:32:14 -0400

    Fix distutils funcs, update base_node_rpc version

--- pavement.py
+++ pavement.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import sys
 
 from paver.easy import task, needs, path, sh, cmdopts, options
-from paver.setuputils import setup, find_package_data
+from paver.setuputils import setup, find_package_data, install_distutils_tasks
 try:
     from base_node_rpc.pavement_base import *
 except ImportError:
@@ -11,6 +11,7 @@ except ImportError:
 sys.path.insert(0, '.')
 import version
 
+install_distutils_tasks()
 
 DEFAULT_ARDUINO_BOARDS = ['uno', 'mega2560']
 PROJECT_PREFIX = [d for d in path('.').dirs()
@@ -34,6 +35,6 @@ options(
                author_email='christian@fobel.net',
                url=URL,
                license='GPLv2',
-               install_requires=['wheeler.base_node_rpc>=0.3'],
+               install_requires=['wheeler.base_node_rpc>=0.4'],
                packages=[PROJECT_PREFIX],
                package_data=package_files))
