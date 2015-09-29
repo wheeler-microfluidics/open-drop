268857c6c4795f862912e70e7d825cb46f3d216e
Ryan Fobel <ryan@fobel.net>
2015-09-29 18:01:54 -0400

    Bump base-node-rpc version

--- pavement.py
+++ pavement.py
@@ -45,6 +45,6 @@ options(
                author_email='christian@fobel.net',
                url=URL,
                license='GPLv2',
-               install_requires=['base-node-rpc>=0.11.post20',
+               install_requires=['base-node-rpc>=0.11.post21',
                                  'arduino-helpers>=0.3.post10'],
                include_package_data=True, packages=[str(PROJECT_PREFIX)]))
